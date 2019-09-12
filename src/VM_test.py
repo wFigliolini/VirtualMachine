f6fb655c4b45fbaeaec933f96c8c5f57d45f9051
Bill <William_Figliolini@student.uml.edu>
2019-09-12 14:20:42 -0400

    Completed Step 6 by converting Tests and adding new Tests for SExprs

--- src/VM_test.py
+++ src/VM_test.py
@@ -17,7 +17,8 @@ def test_1():
     leftIn = 5
     rightIn = 10
     result = leftIn + rightIn
-    testProg = VM.JProg(VM.JAdd(VM.JInt(leftIn), VM.JInt(rightIn)))
+    testString = "+ 5 10"
+    testProg = desugar(testString)
     assert testProg.run() == result
 
 
@@ -25,29 +26,63 @@ def test_2():
     leftIn = 5
     rightIn = 10
     result = leftIn * rightIn
-    testProg = VM.JProg(VM.JMult(VM.JInt(leftIn), VM.JInt(rightIn)))
+    testString = "* 5 10"
+    testProg = desugar(testString)
     assert testProg.run() == result
 
 
 # Recursive cases
 def test_3():
     expr = VM.JMult(VM.JInt(5), VM.JInt(10))
-    testProg = VM.JProg(VM.JAdd(expr, VM.JInt(2)))
+    testString = "+ * 5 10 2"
+    testProg = desugar(testString)
     assert testProg.run() == 52
 
 
 def test_4():
     expr1 = VM.JInt(10)
     expr2 = VM.JAdd(VM.JInt(20), VM.JInt(15))
-    testProg = VM.JProg(VM.JMult(expr1, expr2))
+    testString = "* 10 + 20 15"
+    testProg = desugar(testString)
     assert testProg.run() == 350
 
 
 def test_5():
     expr1 = VM.JMult(VM.JInt(5), VM.JInt(10))
     expr2 = VM.JAdd(VM.JInt(20), VM.JInt(15))
-    testProg = VM.JProg(VM.JMult(expr1, expr2))
+    testString = "* * 5 10 + 20 15"
+    testProg = desugar(testString)
     assert testProg.run() == 1750
+
+
+def test_6():
+    testString = "- 10"
+    testProg = desugar(testString)
+    assert testProg.run() == -10
+
+
+def test_7():
+    testString = "- 10 5"
+    testProg = desugar(testString)
+    assert testProg.run() == 5
+
+
+def test_8():
+    testString = "+"
+    testProg = desugar(testString)
+    assert testProg.run() == 0
+
+
+def test_9():
+    testString = "*"
+    testProg = desugar(testString)
+    assert testProg.run() == 1
+
+
+def test_10():
+    testString = "+ 5"
+    testProg = desugar(testString)
+    assert testProg.run() == 5
 
 
 # Exception Tests
